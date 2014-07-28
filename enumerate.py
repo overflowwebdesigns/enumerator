a2392e8e5084f94f4969e06f59294ffce843417f
maleus <erikjd21@gmail.com>
2014-07-28 18:14:14 -0500

    Added full nmap scan, folder creation

--- enumerate.py
+++ enumerate.py
@@ -13,13 +13,14 @@ import os
 import nmap
 import time
 
-print "Scanning standby..."
-time.sleep(10)
-print "Still Scanning..."
+print "Looking for low hanging fruit hang on..."
+time.sleep(20)
+print "Still lookin, patience young padewan..."
 
 IP = sys.argv[1] # IP address
+os.system("mkdir /root/Desktop/"+IP) # Creates a directory on your Desktop
 nm = nmap.PortScanner() # Initialize Nmap module
-nm.scan(IP, '80,443,22,21') # Target ports
+nm.scan(IP, '80,443,22,21,139,445') # Target ports
 nm.command_line()
 nm.scaninfo()
 	
@@ -40,3 +41,8 @@ for port in lport:
 	print('--------------------')
 	print('port: %s\tstate: %s' % (port, nm[host][proto][port]['state']))
 	print('--------------------')
+
+print "*" * 10
+print "Beginning Service Scan of all ports... Your pwnage can begin soon..."
+print "*" * 10
+os.system("nmap -sV -p- -v -T4 -oN /root/Desktop/"+IP+"/service_scan.txt "+IP) # Full TCP scan of all 65535 ports
