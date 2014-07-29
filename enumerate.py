1da49c02ff03f40c2ea3e6f459eaa064d92cda7c
root <root@localhost.localdomain>
2014-07-28 23:12:08 -0500

    Added FTP anonymous user function

--- enumerate.py
+++ enumerate.py
@@ -12,6 +12,7 @@ import sys
 import os
 import nmap
 import time
+import ftplib
 
 print "Looking for low hanging fruit hang on..."
 time.sleep(20)
@@ -23,7 +24,21 @@ nm = nmap.PortScanner() # Initialize Nmap module
 nm.scan(IP, '80,443,22,21,139,445') # Target ports
 nm.command_line()
 nm.scaninfo()
-	
+
+###########################
+def ftp(): # Attempts to login to FTP using anonymous user
+	try:
+		ftp_info = open('/root/Desktop/'+IP+"ftp_info.txt",'w')
+		ftp = ftplib.FTP(IP)
+		ftp.login()
+		print "\o/"
+		print "FTP ALLOWS ANONYMOUS ACCESS!"
+		print "o/\o"
+		print "*" * 10
+		ftp.quit()
+	except:
+		print "FTP does not allow anonymous access :("
+############################
 
 for host in nm.all_hosts():
 	print('--------------------')
@@ -42,7 +57,12 @@ for port in lport:
 	print('port: %s\tstate: %s' % (port, nm[host][proto][port]['state']))
 	print('--------------------')
 
-print "*" * 10
+if nm[host].has_tcp(21):
+		print "*" * 10
+		print "FTP FOUND - CHECKING FOR ANONYMOUS ACCESS"
+		ftp()
+		
+print "#" * 10
 print "Beginning Service Scan of all ports... Your pwnage can begin soon..."
-print "*" * 10
+print "#" * 10
 os.system("nmap -sV -p- -v -T4 -oN /root/Desktop/"+IP+"/service_scan.txt "+IP) # Full TCP scan of all 65535 ports
