41bb1262bc35069042ce924aeecc4a5b94567209
overflowsec <jtharpe@Justins-MacBook-Pro.local>
2014-08-01 08:59:23 -0400

    Fixed Repo
    Migrated from Maleus repo.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 Enumerator
+
 ==========
 
-Linux Enumeration Script
+This script will do the initial enumeration of a target machine during a pen test.
 
-Dependancies:
+It will look for common ports, and execute additional scanners based on the findings. This is best used in Kali Linux.
 
-This script requires Python-Nmap (http://xael.org/norman/python/python-nmap/)
+Requires Nmap module http://xael.org/norman/python/python-nmap/python-nmap-0.1.4.tar.gz for use.
+
+Usage ./enumerator.py ip.address.here
---
+++ enumerator.py
@@ -0,0 +1,122 @@
+####
+####
+#### Install python Nmap module http://xael.org/norman/python/python-nmap/python-nmap-0.1.4.tar.gz
+####
+####
+####
+####
+#### Usage: ./enumerate.py <ip address>
+####
+####
+####
+#### Author: Maleus
+#### 7.28.14
+####
+
+
+#!/bin/python
+import sys
+import os
+import nmap
+import time
+import ftplib
+import subprocess
+
+if len(sys.argv) != 2:	
+print "Usage ./enumerator.py <ip>"
+sys.exit(1)
+print "Lookin for easy pickins... hang tight"
+IP = sys.argv[1] # IP address
+os.system("mkdir /root/Desktop/"+IP) # Creates a directory on your Desktop
+nm = nmap.PortScanner() # Initialize Nmap module
+nm.scan(IP, '80,443,22,21,139,445') # Target ports
+nm.command_line()
+nm.scaninfo()
+
+###########################
+def ftp(): # Attempts to login to FTP using anonymous user
+try:
+ftp = ftplib.FTP(IP)
+ftp.login()
+print "\o/"
+print "FTP ALLOWS ANONYMOUS ACCESS!"
+print "o/\o"
+ftp.quit()
+except:
+print "FTP does not allow anonymous access :("
+############################
+
+############################
+def dirb(): # Runs dirb on http pages.
+os.system('xterm -hold -e dirb http://'+IP+' -o /root/Desktop/'+IP+'/dirb_info.txt &')
+print 'Running Dirb on port 80 - Check the target folder for output file.'
+############################
+
+###########################
+def dirb_https(): # Runs dirb on https pages.
+os.system('xterm -hold -e dirb https://'+IP+' -o /root/Desktop/'+IP+'/dirb_https_info.txt &')
+print 'Running Dirb on port 443 - Check the target folder for output file.'
+###########################
+
+###########################
+def enum4linux(): # Runs enum4linux on the target machine.
+proc = subprocess.Popen('enum4linux '+IP+' > /root/Desktop/'+IP+'/smb_info.txt &', shell = True)
+stdout,stderr = proc.communicate()
+print 'Beginning enum4linux - this may take a few minutes to complete. - Info will be available in the smb_info.txt file -'
+
+###########################
+
+###########################
+def nikto():
+os.system('xterm -hold -e nikto -host http://'+IP+' -output /root/Desktop/'+IP+'/nikto_http.txt &')
+print 'Running Nikto against port 80 - Check target folder for output file.'
+###########################
+
+###########################
+def nikto_https():
+os.system('xterm -hold -e nikto -host https://'+IP+' -output /root/Desktop/'+IP+'/nikto_https.txt &')
+print 'Running Nikto against port 443 - Check target folder for output file.'
+###########################
+
+
+for host in nm.all_hosts():
+print('--------------------')
+print('Host: %s (%s)' % (IP, nm[host].hostname()))
+print('State: %s' % nm[host].state())
+print('--------------------')
+
+for proto in nm[host].all_protocols():
+print('--------------------')
+print('Protocol: %s' % proto)
+
+lport = nm[host]['tcp'].keys()
+lport.sort()
+for port in lport:
+print('--------------------')
+print('port: %s\tstate: %s' % (port, nm[host][proto][port]['state']))
+print('--------------------')
+
+# Function Checks
+if nm[host].has_tcp(21) and nm[IP]['tcp'][21]['state'] == 'open':
+print "*" * 10
+print "FTP Found - Checking for anonymous access."
+ftp()
+if nm[host].has_tcp(80) and nm[IP]['tcp'][80]['state'] == 'open':
+dirb()
+if nm[host].has_tcp(443) and nm[IP]['tcp'][443]['state'] == 'open':
+dirb_https()
+if nm[host].has_tcp(80) and nm[IP]['tcp'][80]['state'] == 'open':
+nikto()
+if nm[host].has_tcp(443) and nm[IP]['tcp'][443]['state'] == 'open':
+nikto_https()
+if nm[host].has_tcp(139) and nm[IP]['tcp'][139]['state'] == 'open' or nm[host].has_tcp(445) and nm[IP]['tcp'][445]['state'] == 'open':
+        enum4linux()
+
+#Nmap Service Scan
+print "Beginning Service Scan of all ports... Your pwnage can begin soon..."
+print "#" * 10
+os.system("nmap -A -p- -T4 -oN /root/Desktop/"+IP+"/service_scan.txt "+IP) # Full TCP scan of all 65535 ports
+
+
+
+
